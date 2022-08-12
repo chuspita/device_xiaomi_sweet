41ca794b7eed85e2b4fa4a416b682061b8930353
Niranjan BR <niranjankannan2003@gmail.com>
2022-08-12 00:27:10 +0000

    sweet: vendorsetup.sh: Track customised Vantom Kernel
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -23,4 +23,4 @@ git clone -b twelve https://gitlab.com/mrfox2003/vendor_xiaomi_sweet-miuicamera.
 
 #Kernel
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
-git clone -b courbet-12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b courbet-12.1 --depth=1 https://github.com/PixelOS-Devices/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150

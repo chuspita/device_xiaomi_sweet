80c1ae7107fba5ab662d19f1d1dae6401cf106cd
Niranjan BR <niranjankannan2003@gmail.com>
2022-09-16 12:51:41 +0000

    sweet: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@
 rm -rf hardware/qcom-caf/sm8150/display 
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
+rm -rf packages/resources/devicesettings
 
 # Hals
 git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/audio 
@@ -15,9 +16,8 @@ git clone https://github.com/Sm6150-Sweet/device_xiaomi_sm6150 -b 13.0 device/xi
 git clone https://github.com/Sm6150-Sweet/vendor_xiaomi.git -b 13.0 vendor/xiaomi
 
 # Device Settings
-rm -rf packages/resources/devicesettings
 git clone https://github.com/PixelExperience/packages_resources_devicesettings packages/resources/devicesettings
 
 # Kernel
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
-git clone -b courbet-13 --depth=1 https://github.com/PixelOS-Devices/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
+git clone -b meraki --depth=1 https://github.com/Sm6150-Sweet/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
